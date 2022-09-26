4258daf1920c25991e51aeb989f2ca5b4ae09ddf
Tomáš Hnídek <tomas.hnidek@monster.com>
2022-09-26 17:26:58 +0200

    chore: design update

--- components/item-list/item-list.style.ts
+++ components/item-list/item-list.style.ts
@@ -3,4 +3,5 @@ import styled from '@emotion/styled';
 export const ItemListStyle = styled.div`
   display: flex;
   flex-wrap: wrap;
+  place-content: space-around;
 `;
--- components/layout/layout.style.ts
+++ components/layout/layout.style.ts
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 export const Main = styled.main`
   margin: 0 auto;
-  width: 80%;
+  width: 82%;
   text-align: center;
 `;
